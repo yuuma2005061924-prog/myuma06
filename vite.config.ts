88f77507d9c5a62822f049c36f21590b36cb041e
yuuma2005061924-prog <yuuma2005061924@gmail.com>
2026-04-10 01:46:56 +0000

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@theme/v2/tailwindcss'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/myuma06/', // ← この行を必ず追加してください！最後はカンマです。
 })
